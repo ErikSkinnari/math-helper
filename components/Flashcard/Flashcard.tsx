67689b9ed41cb83fd6ed738832a2cf0d1ef24221
Johanna Skinnari <jskinnari@gmail.com>
2021-08-31 22:56:03 +0200

    #11-fix-eslint

--- components/Flashcard/Flashcard.tsx
+++ components/Flashcard/Flashcard.tsx
@@ -1,14 +1,20 @@
 import React , { useState } from 'react'
+
 import { FlashcardProps } from './types'
 
 const Flashcard = ({ flashcard }: FlashcardProps): JSX.Element => {
-    const [flip, setFlip] = useState(false)
+  const [flip, setFlip] = useState(false)
 
-    return (
-        <div className={`${flip ? 'bg-indigo-100' : 'bg-gray-100'} max-w-sm rounded overflow-hidden shadow-lg w-full px-6 py-4 font-bold text-6xl flex justify-center items-center  h-56`} onClick={() => setFlip(!flip)}>
-            {flip ? flashcard.answer : flashcard.question }
-        </div>
-    )
+  return (
+    <div 
+    // eslint-disable-next-line max-len
+      className={`${flip ? 'bg-indigo-100' : 'bg-gray-100'} max-w-sm rounded overflow-hidden shadow-lg w-full px-6 py-4 font-bold text-6xl flex justify-center items-center  h-56`} 
+      onClick={() => setFlip(!flip)}
+    >
+      {flip ? flashcard.answer : flashcard.question }
+    </div>
+  )
 }
+
 
 export default Flashcard
--- components/Flashcard/FlashcardList.tsx
+++ components/Flashcard/FlashcardList.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
+
 import Flashcard from './Flashcard'
 import { FlashcardsProps } from './types'
 
-const FlashcardList = ({ flashcards }: FlashcardsProps ): JSX.Element => {
-    return (
-        <div>
-            {flashcards.map(flashcard => {
-                return <Flashcard flashcard={flashcard} key={flashcard.id} />
-            })}
-        </div>
-    )
+const FlashcardList = ({ flashcards }: FlashcardsProps): JSX.Element => {
+  return (
+    <div>
+      {flashcards.map(flashcard => {
+        return (
+          <Flashcard
+            flashcard={flashcard}
+            key={flashcard.id}
+          />
+        )
+      })}
+    </div>
+  )
 }
 
 export default FlashcardList
